e105a79d0f3cf8cf933e4b637a6ae240b4728501
Stanisław Szewerda <stanislaw.szewerda@gmail.com>
2022-08-18 22:04:42 +0200

    Add cubit and state for restaurant_page_content

---
+++ lib/App/Home/restaurants/cubit/restaurants_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:meta/meta.dart';
+
+part 'restaurants_state.dart';
+
+class RestaurantsCubit extends Cubit<RestaurantsState> {
+  RestaurantsCubit()
+      : super(const RestaurantsState(
+            documents: [], isLoading: false, errorMessage: ''));
+}
--- lib/App/Home/restaurants/restaurants_page_content.dart
+++ lib/App/Home/restaurants/restaurants_page_content.dart
@@ -1,5 +1,7 @@
+import 'package:burgery_opole/App/Home/restaurants/cubit/restaurants_cubit.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class RestaurantPageContent extends StatelessWidget {
   const RestaurantPageContent({
@@ -8,21 +10,23 @@ class RestaurantPageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream: FirebaseFirestore.instance
-            .collection('restaurants')
-            .orderBy('rating', descending: true)
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Center(child: Text('Something went wrong'));
+    return BlocProvider(
+      create: (context) => RestaurantsCubit(),
+      child: BlocBuilder<RestaurantsCubit, RestaurantsState>(
+        builder: (context, state) {
+          if (state.errorMessage.isNotEmpty) {
+            return Center(
+              child: Text(
+                'Something went wrong ${state.errorMessage}',
+              ),
+            );
           }
-          if (snapshot.connectionState == ConnectionState.waiting) {
+          if (state.isLoading == true) {
             return const Center(
                 child: Text('Proszę czekać trwa ładowanie danych'));
           }
 
-          final documents = snapshot.data!.docs;
+          final documents = state.documents;
 
           return ListView(children: [
             for (final document in documents) ...[
@@ -44,6 +48,46 @@ class RestaurantPageContent extends StatelessWidget {
               )
             ],
           ]);
-        });
+
+          return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+              stream: FirebaseFirestore.instance
+                  .collection('restaurants')
+                  .orderBy('rating', descending: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return const Center(child: Text('Something went wrong'));
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                      child: Text('Proszę czekać trwa ładowanie danych'));
+                }
+
+                final documents = snapshot.data!.docs;
+
+                return ListView(children: [
+                  for (final document in documents) ...[
+                    Padding(
+                      padding: const EdgeInsets.all(40.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(document['name']),
+                              Text(document['burger']),
+                            ],
+                          ),
+                          Text(document['rating'].toString()),
+                        ],
+                      ),
+                    )
+                  ],
+                ]);
+              });
+        },
+      ),
+    );
   }
 }
