9d8f5db90b896590b7ca991c345df9a909fb5aac
Stanisław Szewerda <stanislaw.szewerda@gmail.com>
2022-08-20 14:32:25 +0200

    Add event start

--- lib/App/Home/restaurants/restaurants_page_content.dart
+++ lib/App/Home/restaurants/restaurants_page_content.dart
@@ -1,5 +1,4 @@
 import 'package:burgery_opole/App/Home/restaurants/cubit/restaurants_cubit.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -11,7 +10,7 @@ class RestaurantPageContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => RestaurantsCubit(),
+      create: (context) => RestaurantsCubit()..start(),
       child: BlocBuilder<RestaurantsCubit, RestaurantsState>(
         builder: (context, state) {
           if (state.errorMessage.isNotEmpty) {
@@ -22,8 +21,7 @@ class RestaurantPageContent extends StatelessWidget {
             );
           }
           if (state.isLoading == true) {
-            return const Center(
-                child: Text('Proszę czekać trwa ładowanie danych'));
+            return const Center(child: CircularProgressIndicator());
           }
 
           final documents = state.documents;
@@ -48,44 +46,6 @@ class RestaurantPageContent extends StatelessWidget {
               )
             ],
           ]);
-
-          return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-              stream: FirebaseFirestore.instance
-                  .collection('restaurants')
-                  .orderBy('rating', descending: true)
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return const Center(child: Text('Something went wrong'));
-                }
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                      child: Text('Proszę czekać trwa ładowanie danych'));
-                }
-
-                final documents = snapshot.data!.docs;
-
-                return ListView(children: [
-                  for (final document in documents) ...[
-                    Padding(
-                      padding: const EdgeInsets.all(40.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(document['name']),
-                              Text(document['burger']),
-                            ],
-                          ),
-                          Text(document['rating'].toString()),
-                        ],
-                      ),
-                    )
-                  ],
-                ]);
-              });
         },
       ),
     );
